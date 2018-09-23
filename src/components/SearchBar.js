564c2306e716fc450cbcd68ffb38a7088e1c4fa6
Taryn Sauer <taryn.sauer@gmail.com>
2018-09-23 13:58:30 -0500

    wip - add autocomplete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Autocomplete from 'react-autocomplete'
 
 class SearchBar extends Component {
+  state = {
+    value: '',
+    suggestedValues: [],
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value})
+    //Get new suggestedValues from API
+  }
+
+  handleSelect(value) {
+    this.setState({value: value})
+    //Navigate to the airport's show page
+  }
+
   render() {
     return (
       <div>
-        <input />
+        <Autocomplete
+          getItemValue={(item) => item.name}
+          items={this.state.suggestedValues}
+          renderItem={(item, isHighlighted) =>
+              <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                {item.name}
+              </div>
+          }
+          value={this.state.value}
+          onChange={this.handleChange.bind(this)}
+          onSelect={this.handleSelect.bind(this)}
+        />
       </div>
     )
   }
