40e021a8d61fc052a55e27a8175843c4d71918c1
Taryn Sauer <taryn.sauer@gmail.com>
2018-09-24 23:53:25 -0500

    feat: Add 404 page handling

--- src/components/AirportsIndex.js
+++ src/components/AirportsIndex.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import AirportPage from './AirportPage'
 import Categories from './Categories'
 import HighTraffic from './HighTraffic'
+import NotFound from './NotFound'
 import SearchBar from './SearchBar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 import '../App.css'
 
 class AirportsIndex extends Component {
@@ -15,12 +16,17 @@ class AirportsIndex extends Component {
       <BrowserRouter>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">SkyGod</h1>
+            <Link to='/'>
+              <h1 className="App-title">SkyGod</h1>
+            </Link>
             <SearchBar token={token} airports={airports} />
           </header>
-          <Route path='/' exact render={(props) => <Categories airports={airports} />} />
-          <Route path='/airports/:id' component={AirportPage} />
-          <Route path='/high-traffic' render={(props) => <HighTraffic airports={airports} />} />
+          <Switch>
+            <Route path='/' exact render={(props) => <Categories airports={airports} />} />
+            <Route path='/airports/:id' component={AirportPage} />
+            <Route path='/high-traffic' render={(props) => <HighTraffic airports={airports} />} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </BrowserRouter>
     )
